45c7f03142794b30f343f06ea042eefeaf139568
Marcos Pereira <engmarcospfilho@gmail.com>
2025-04-24 11:08:09 -0300

    botao fontes etc
    mais atualizacoes no projeto bolo

--- sugar_rush/sugar-rush-project/src/main.jsx
+++ sugar_rush/sugar-rush-project/src/main.jsx
@@ -2,6 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 
+function Menu(){
+  return(
+    <ul className = "menu">
+      <li className='menuitem'><a href="../../index.html">Home</a></li>
+      <li id="barramenu" className='menuitem'><a href="../../pages/carrinho.html">Carrinho</a></li>
+    </ul>
+  )
+}
+function Header(){
+  return(
+    <header className="ralewayfont">
+      <h1>Sugar Rush</h1>
+      <Menu/>
+    </header>
+  )
+}
 function AdicionarProduto(){
   return(
     <div className='inputbox'>
@@ -17,48 +33,29 @@ function AdicionarProduto(){
         <label for="desc" className='ralewayfont'>Descrição</label>
         <input type="text" id="desc" name="desc" placeholder="Um pequeno bolo" className='ralewayfont'/>
       </div>
+      <button class="inputitem"><i class="fas fa-plus"></i></button>
     </div>
   )
 }
-
 function CupcakeItem({ className = "" }){
     return(
-      <figure>
-        <img src="../../extra/icon _cupcake_.png" className={`cupitem ${className}`}/>
+      <figure className={`cupitem ${className}`}>
+        <img src="../../extra/icon _cupcake_.png" />
+        <p className='ralewayfont cuptxt'>Cupcake</p>
       </figure>
     )
 }
-
-function Menu(){
-  return(
-    <ul className = "menu">
-      <li className='menuitem'><a href="../../index.html">Home</a></li>
-      <li id="barramenu" className='menuitem'><a href="../../pages/carrinho.html">Carrinho</a></li>
-    </ul>
-  )
-}
-function Header(){
-  return(
-    <header>
-      <h1>Sugar Rush</h1>
-      <Menu/>
-    </header>
-  )
-}
-
 function Main(){
   return(
-    <main>
+    <main className="ralewayfont">
       <section className='flexitem' id='sec1'>
         <h1 className="ralewayfont">Welcome</h1>
         <h2 className="ralewayfont">CRAVING SUGAR?</h2>
       </section>
       <section className='flexitem' id='sec2'>
-        <h1>
-          <CupcakeItem className="discount"/>
-          <CupcakeItem className="cupitem"/>
-          <CupcakeItem className="cupitem"/>
-        </h1>
+        <CupcakeItem className="discount"/>
+        <CupcakeItem className="cupitem"/>
+        <CupcakeItem className="cupitem"/>
       </section>
       <section className='flexitem' id='sec3'>
         <AdicionarProduto/>
@@ -66,9 +63,10 @@ function Main(){
     </main>
   )
 }
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Header className="ralewayfont"/>
-    <Main className="ralewayfont"/>
+    <Header/>
+    <Main/>
   </StrictMode>,
 )
